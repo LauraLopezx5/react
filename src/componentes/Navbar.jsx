488a795a7417ecbcb7d4240abbf93c1c41c6e1cd
lauralopez <laura_x5@hotmail.com>
2022-07-19 22:17:31 -0300

    primer commit

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Inicio</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Seccion 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Seccion 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Seccion 3</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Seccion 4
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">1</a></li>
+                  <li><a class="dropdown-item" href="#">2</a></li>
+                  <li><a class="dropdown-item" href="#">3</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
