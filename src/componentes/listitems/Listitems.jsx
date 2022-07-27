f52f90383143bc7dfaec50eafd78de6fac4ae794
lauralopez <laura_x5@hotmail.com>
2022-07-26 21:01:18 -0300

    itemcount

--- src/componentes/listitems/Listitems.jsx
+++ src/componentes/listitems/Listitems.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+// import React from 'react'
 import Card from './Card'
 
 const Listitems = () => {
   return (
-    <div class = "d-flex flex-wrap justify-content-around">
-        <Card nombre = "Producto 1"/>
-        <Card nombre = "Producto 2"/>
-        <Card nombre = "Producto 3"/>
+    <div className = "d-flex flex-wrap justify-content-around">
+        <Card id = "1" stock = "7"/>
+        {/* <Card id = "2" stock = "3"/>
+        <Card id = "3" stock = "5"/> */}
 
     </div>
   )
--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 import Cartwidge from './Cartwidge'
+import './navbar.css'
 
 const Navbar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Inicio</a>
-          <a href="#" class="navbar-brand"> <Cartwidge/> </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className ="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className ="container-fluid">
+          <a className ="navbar-brand" href="#">Inicio</a>
+          <a href="#" className="navbar-brand"> <Cartwidge/> </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Seccion 1</a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Seccion 1</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Seccion 2</a>
+              <li className ="nav-item">
+                <a className="nav-link" href="#">Seccion 2</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Seccion 3</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Seccion 3</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Seccion 4
                 </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">1</a></li>
-                  <li><a class="dropdown-item" href="#">2</a></li>
-                  <li><a class="dropdown-item" href="#">3</a></li>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">1</a></li>
+                  <li><a className="dropdown-item" href="#">2</a></li>
+                  <li><a className="dropdown-item" href="#">3</a></li>
                 </ul>
               </li>
             </ul>
